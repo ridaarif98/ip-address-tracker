7190cb34006a2efe3d565c1034cd3f145f10c947
ridaarif <ridaarif16@gmail.com>
2024-06-05 12:34:56 +0200

    Remove map from app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import { MapContainer, TileLayer,  Marker, Popup } from 'react-leaflet';
-import "leaflet/dist/leaflet.css"
+import React  from 'react';
 import './App.css';
 import './form.scss'
 import IPForm from './Form';
 
 function App() {
-  const position = [51.505, -0.09]
   return (
     <>
       <IPForm />
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "50vh", width: "100%" }}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
     </>
   );
 }
